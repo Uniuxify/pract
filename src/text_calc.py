adbf00a9a3cafeafe1abe0e6c627876011df113d
Uniuxify <Uniuxify@gmail.com>
2024-02-12 11:18:15 +0300

    Removed extra spaces

--- src/text_calc.py
+++ src/text_calc.py
@@ -1,16 +1,14 @@
 class Clac:
-
     def __init__(self):
         pass
-        
-        
+
     def parse(self, expr):
         pass
-    
-    
+
 
 def main():
     pass
-    
+
+
 if __name__ == '__main__':
     main()
