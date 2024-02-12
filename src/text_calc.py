cffec668c4d7d73d35a5f7df3ee21e60ffe45d62
Uniuxify <Uniuxify@gmail.com>
2024-02-12 11:16:02 +0300

    Added operations

--- src/text_calc.py
+++ src/text_calc.py
@@ -2,12 +2,31 @@ class Clac:
 
     def __init__(self):
         pass
-        
-        
-    def parse(self, expr):
-        pass
-    
-    
+
+    def _parse(self, expr):
+        if '+' in expr:
+            op = '+'
+        elif '-' in expr:
+            op = '-'
+        elif '*' in expr:
+            op = '*'
+        else:
+            op = '/'
+
+        a, b = expr.split(op)
+        return a, op, b
+
+    def calc(self, expr):
+        a, op, b = self._parse(expr)
+        if op == '+':
+            return a + b
+        if op == '*':
+            return a * b
+        if op == '/':
+            return a / b
+        if op == '-':
+            return a - b
+
 
 def main():
     pass
