38e7b27a5cf378b35fecf1392b03d9c12c7da8f7
Uniuxify <Uniuxify@gmail.com>
2024-02-12 10:46:18 +0300

    Basic program structure

---
+++ src/text_calc.py
@@ -0,0 +1,16 @@
+class Clac:
+
+    def __init__(self):
+        pass
+        
+        
+    def parse(self, expr):
+        pass
+    
+    
+
+def main():
+    pass
+    
+if __name__ == '__main__':
+    main()
